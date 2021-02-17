bee621339646bfa97ebaa947f9108963e219e29d
Iago <iago.gc88@gmail.com>
2021-02-17 11:29:27 +0100

    Peliculas_SAlas

---
+++ NET_API_MODELS_Peliculas/Models/Pelicula.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NET_API_MODELS_Peliculas.Models {
+    public class Pelicula {
+        private int _codigo;
+        private string _nombre = string.Empty;
+        private int _calificacionEdad;
+        private ICollection<Sala> _salas;
+
+        public int Codigo {
+            get => _codigo;
+            set => _codigo = value;
+        }
+        public string Nombre {
+            get => _nombre;
+            set => _nombre = value;
+        }
+        public int CalificacionEdad {
+            get => _calificacionEdad;
+            set => _calificacionEdad = value;
+        }
+        public ICollection<Sala> Salas {
+            get => _salas;
+            set => _salas = value;
+        }
+    }
+}
---
+++ NET_API_MODELS_Peliculas/Models/Sala.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NET_API_MODELS_Peliculas.Models {
+    public class Sala {
+        private int _codigo;
+        private string _nombre;
+        private Pelicula _pelicula;
+        public int Codigo {
+            get => _codigo;
+            set => _codigo = value;
+        }
+        public string Nombre {
+            get => _nombre;
+            set => _nombre = value;
+        }        
+        public Pelicula Pelicula {
+            get => _pelicula;
+            set => _pelicula = value;
+        }
+    }
+}
